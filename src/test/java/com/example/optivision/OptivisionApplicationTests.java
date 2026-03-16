b45e08599fb0ddbd851f085305b04c6a6ad7cf51
PhamDoTieuPhuong <2251120377@ut.edu.vn>
2026-03-16 20:15:45 +0700

    Home Page

---
+++ src/test/java/com/example/optivision/OptivisionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.optivision;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OptivisionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
